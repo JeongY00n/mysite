415ab50fff936c68d2c91660ce5213aa8261190d
JeongY00n <shimmumu@gmail.com>
2022-01-11 16:19:20 +0900

    second commit

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,46 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+from .models import Question
+#명심! Client에서 request를 받게 되면 여기에는 여러가지 정보가 담겨있을 것이고 다시 response를 해준다
+#response를 해주기 전에 데이터를 추출하거나 저장하거나 파일다운을 하기 위한 로직을 작성할수도있다
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello, world!")
+    #1
+    #return HttpResponse("Hello, world!")
+    
+    #2
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    
+    #3
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    #context = {
+    #    'latest_question_list': latest_question_list,
+    #}
+    #return HttpResponse(template.render(context, request))
+
+    #4
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+def detail(request, question_id):
+    #1
+    # return HttpResponse("You're looking at question %s." % question_id)
+    #2
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
